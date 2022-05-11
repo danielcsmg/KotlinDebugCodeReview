7bd6b732b2f8ab8fedfe541a632a1bc8d86ed4a2
danielcsmg <daniel.csouzamg@gmail.com>
2022-05-10 22:21:54 -0300

    Correções do exercício 1

--- src/estoque/Estoque.kt
+++ src/estoque/Estoque.kt
@@ -1,38 +1,57 @@
 package src.estoque
 
 import src.item.Item
+import java.math.BigDecimal
 
 class Estoque {
     private val listaItem: ArrayList<Item> = ArrayList()
 
     fun registrarItem() {
-        println("Digite o código do produto: ")
-        val codigo = readln().toInt()
-        println("Digite o nome do produto: ")
-        val nomeItem = readln()
-        println("Digite o preco do produto: ")
-        val preco = readln().toBigDecimal()
-        val item: Item = Item(codigo = codigo, nome = nomeItem, preco = preco)
-        listaItem.add(item);
+        val codigo: Int
+        val preco: BigDecimal
+        val nomeItem: String
+        try {
+            println("Digite o código do produto: ")
+            codigo = readln().toInt()
+            println("Digite o nome do produto: ")
+            nomeItem = readln()
+            println("Digite o preco do produto: ")
+            preco = readln().toBigDecimal()
+        }catch (e: Exception){
+            println("Erro ao tentar cadastrar o produto!")
+            println("Verifique os formatos do código e do preço:")
+            return
+        }
+        val item: Item = Item(verificarItemRepetido(codigo), nomeItem, preco)
+        listaItem.add(item)
         println("Item registrado com sucesso\n")
 
     }
 
-    fun verificarItemRepetido() {
-        TODO("Não implementado ainda")
+    private fun verificarItemRepetido(codigo: Int): Int {
+        var codigoComparador = codigo
+        listaItem.forEach{
+            while(it.codigo == codigoComparador){
+                println("Códigos já usado! ")
+                println("Digite o código do produto: ")
+                codigoComparador = readln().toInt()
+            }
+        }
+        return codigoComparador
     }
 
-    private fun listarItens() {
-        if (!listaItem.isEmpty()) {
+    fun listarItens() {
+        if (listaItem.isEmpty()) {
             println("Não temos nenhum item cadastrado no momento")
         } else {
-            println("Atualmente temos os seguintes itens: ${listaItem.toString()}");
+            println("Atualmente temos os seguintes itens:")
+            listaItem.toString()
         }
     }
 
     fun darBaixaItem() {
         print("Qual o código do item a dar baixa? ")
-        val codigoItemADarBaixa = readln()
+        val codigoItemADarBaixa = readln().toInt()
         for (item in listaItem) {
             if (codigoItemADarBaixa == item.codigo) {
                 listaItem.remove(item)
--- src/menu/Menu.kt
+++ src/menu/Menu.kt
@@ -12,28 +12,32 @@ class Menu {
         println("3 - Dar baixa em um item")
         println("4 - Sair do programa")
 
-        //TODO:deixar sem o try/catch para avaliar se a galera percebe que dá problema de entrada de dado se for string
-        val opcao = readln().toInt()
+        try {
+            val opcao = readln().toInt()
 
-        when (opcao) {
-            1 -> {
-                estoque.registrarItem()
-                this.menuPrincipal(estoque)
-            }
-            2 -> {
-                estoque.listarItens()
-                this.menuPrincipal(estoque)
-            }
-            3 -> {
-                estoque.darBaixaItem()
-                this.menuPrincipal(estoque)
-            }
+            when (opcao) {
+                1 -> {
+                    estoque.registrarItem()
+                    this.menuPrincipal(estoque)
+                }
+                2 -> {
+                    estoque.listarItens()
+                    this.menuPrincipal(estoque)
+                }
+                3 -> {
+                    estoque.darBaixaItem()
+                    this.menuPrincipal(estoque)
+                }
 
-            4 -> exitProcess(10)
-            else -> {
-                println("Opção inválida, tente novamente")
-                menuPrincipal(estoque);
+                4 -> exitProcess(0)
+                else -> {
+                    println("Opção inválida, tente novamente")
+                    menuPrincipal(estoque);
+                }
             }
+        }catch (e: Exception){
+            println("Opção inválida, digite novamente")
+            menuPrincipal(estoque)
         }
     }
 }
